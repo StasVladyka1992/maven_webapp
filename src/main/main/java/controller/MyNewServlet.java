21352bee3efd971b0cf988a2201859ae5be378a6
Stas Vladyka <stavladyk@gmail.com>
2019-04-01 20:38:17 +0300

    1) tomcat and maven settings were changed (tomcat-users.xml and settings.xml). 2) tomcat plugin compiler was added to pom.xml 3) command for building and deploy "mvn tomcat7:redeploy" 4) maven archetype:  "mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-webapp -DarchetypeVersion=1.4"

---
+++ src/main/main/java/controller/MyNewServlet.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Vladyka Stas
+ * on 01.04.2019 at 19:26
+ **/
+public class MyNewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/hidden.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/hidden.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+</head>
+<body>
+<p>
+    Main page
+</p>
+
+<a href="/maven_webapp/hidden">To hidden page</a>
+</body>
+</html>
